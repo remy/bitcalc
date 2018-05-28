d92c0ba67ae9e19e9ed882454b5195b0b8e30e2a
Remy Sharp <remy@remysharp.com>
2018-05-28 09:21:47 +0100

    feat: add endian support

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const $ = s => document.querySelector(s);
 
 const tape = $('#tape');
 const byteSizeEl = $('#byte-size');
+const endianEl = $('#endian');
+let bigEndian = true;
 let byteSize = byteSizeEl.value;
 let signed = false;
 let input = $('input');
@@ -12,7 +14,14 @@ let result = $('.result');
 let ptr = 0;
 let results = new DataView(new ArrayBuffer(1024));
 
+endianEl.onchange = () => {
+  const v = (localStorage.endian = endianEl.value);
+  bigEndian = v === 'big';
+  run();
+};
+
 byteSizeEl.onchange = () => {
+  localStorage.byteSize = byteSizeEl.value;
   byteSize = parseInt(byteSizeEl.value, 10);
   signed = byteSizeEl.selectedOptions[0].dataset.signed === '1';
   run();
@@ -21,15 +30,15 @@ byteSizeEl.onchange = () => {
 function printValue(value) {
   return (
     toBinary(value, byteSize)
-    .split('')
-    .reduce((acc, curr, i) => {
-      if (i > 0 && i % 4 === 0) {
-        acc.push(' ');
-      }
-      acc.push(curr);
-      return acc;
-    }, [])
-    .join('') +
+      .split('')
+      .reduce((acc, curr, i) => {
+        if (i > 0 && i % 4 === 0) {
+          acc.push(' ');
+        }
+        acc.push(curr);
+        return acc;
+      }, [])
+      .join('') +
     `<span class="decimal">(${value} 0x${toHex(value, byteSize)})</span>`
   );
 }
@@ -42,7 +51,7 @@ const run = (e = { target: { nodeName: 'INPUT' } }) => {
   const intType = signed ? 'Int' : 'Uint';
 
   try {
-    results[`set${intType}${byteSize}`](ptr, eval(input.value));
+    results[`set${intType}${byteSize}`](ptr, eval(input.value), !bigEndian);
   } catch (e) {
     console.error(e);
   }
@@ -76,6 +85,16 @@ body.onkeydown = e => {
     run();
   }
 };
+
+if (localStorage.byteSize) {
+  byteSizeEl.value = localStorage.byteSize;
+  byteSizeEl.onchange();
+}
+
+if (localStorage.endian) {
+  endianEl.value = localStorage.endian;
+  endianEl.onchange();
+}
 
 run();
 input.selectionStart = input.selectionEnd = input.value.length;
